3466282f5d3d0b5f808664e88807409ba2b7f195
Jessy-Jones <93819297+Jessy-Jones@users.noreply.github.com>
2024-06-13 16:50:42 +0000

    Jokes with React

---
+++ src/Jokes.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+export default function Jokes(props){
+    return(
+        <div>
+            <h3>{props.setup}</h3>
+            <p>{props.punchline}</p>
+            <hr/>
+        </div>
+    )
+}
