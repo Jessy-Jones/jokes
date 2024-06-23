47745c5ee775b3b98c61a1f12943de208608006f
Jessy-Jones <93819297+Jessy-Jones@users.noreply.github.com>
2024-06-23 14:01:50 +0000

    Using Map to make things easier

--- src/App.js
+++ src/App.js
@@ -1,30 +1,21 @@
 import React from "react"
 import Jokes from "../src/Jokes"
+import jokesData from "./jokesData"
+
+
 
 
 export default function App(){
+  const jokeElement = jokesData.map(joke => {
+    return <Jokes 
+    setup={joke.setup}
+    punchline={joke.punchline}/>
+  })
   return(
-    <>
-    <Jokes
-    setup="I got my daughter a fridge for her birthday" 
-    punchline="I can't wait to see her face light up when she opens it."
-    />
-    <Jokes
-    setup="How did the hacker escape the police?"
-    punchline="He just ransomware."
-    />
-    <Jokes
-    setup="Why don't pirates travel on mountain roads?"
-    punchline="Scurvy"
-    />
-    <Jokes
-    setup="Why do bees stay in the hive in the winter?"
-    punchline="Swarm"
-    />
-    <Jokes
-    setup="What's the best thing about Switzerland?"
-    punchline="I don't kmow but the flag is big"
-    />
-    </>
+    <div>
+       {jokeElement}
+    </div>
+   
+    
   )
 }
